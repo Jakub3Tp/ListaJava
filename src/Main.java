3d1c47574dfb401922dddb954a0ec39b821ae105
student06 <jakub.gola2@teb.edu.pl>
2024-10-31 11:24:45 +0100

    commit 31.10.2024

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        MyList<Integer> list = new MyList<>();
+        System.out.println("Podaj wartości do wpisania na listę. " +
+                "Wpisanie -1 powoduje koniec wczytywania");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        int value;
+        while(true) {
+            value = scanner.nextInt();
+            list.addElement(value);
         }
+        while(value!=-1);
+        System.out.println("--------------");
+        list.display();
+
+        list.addElement(1);
+        System.out.println("--------------");
+        list.display();
+
+        list.addElement(10);
+        System.out.println("--------------");
+        list.display();
+
+        list.removeElement();
+        System.out.println("--------------");
+        list.display();
     }
 }
--- src/MyList.java
+++ src/MyList.java
@@ -1,7 +1,61 @@
+import javax.swing.*;
+
 public class MyList<T> {
     private Node<T> head;
 
+    //dodaje element
     public void addElement(T value){
+        Node<T> node = new Node<>(value, null);
+        if(head==null){
+            head = node;
+        }else{
+            Node<T> element = head;
+            while(element.next != null){
+                element = element.next;
+            }
+            element.next = node;
+        }
+    }
 
+    // wyświetla wszystkie elementy listy
+    public void display(){
+        Node<T> node = head;
+            if (head != null) {
+                    while (node != null) {
+                        System.out.println(node.value);
+                        node = node.next;
+                    }
+                }
+    }
+
+    //usuwa ostatni element list
+    public void removeElement(){
+        Node<T> node = head;
+            if (head != null) {
+                Node<T> prev = head;
+                while (head != null){
+                    prev = head;
+                    node = head.next;
+                }
+                if (prev != node) {
+                    prev.next = null;
+                }else{
+                    head = null;
+                }
+            }
+    }
+
+    //wyszukiwanie elementu
+    public  Node<T> getElement(T value){
+        Node<T> node = head;
+        if (node != null) {
+            while (node != null) {
+                if (node.value.equals(value)){
+                    return node;
+                }
+                node = node.next;
+            }
+        }
+        return null;
     }
 }
